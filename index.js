9de2cf16f35bf443aeeb9a74508bdd0a13ef0dac
Justen Anderson <justenanderson@gmail.com>
2023-03-25 10:45:56 -0600

    Initial set-up

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// Test comment
-const x = 1
-const y = 2
-console.log('Hello World')
+
+var removeElement = function(nums, val) {
+  // Given an array and a number, search the array for the number and remove any instances of that number.
+  // Search 
+  // Remove 
+  // return nums.length so the test can check to see if I did it correctly
+  console.log('Hello world!')
+};
+
+// Case 1
+const nums1 = [0,1,2,2,3,0,4,2]
+const val1 = 2
+removeElement(nums1, val1)
+// Expected output = [0,1,4,0,3]
+
+// Case 2
+const nums2 = [3,2,2,3]
+const val2 = 3
+removeElement(nums2, val2)
+// Expected output = [2,2]
