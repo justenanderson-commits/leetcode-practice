9e182d9086ed39b40ae3ded7c657e31f789c530a
Justen Anderson <justenanderson@gmail.com>
2023-03-26 12:51:29 -0600

    Add solution to remove duplicates.

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-// Remove Elements Problem: 
+// Remove duplicates
+let nums1 = [1,1,2]
+let nums2 = [0,0,1,1,1,2,2,3,3,4]
+// Expected output = [0,1,2,3,4}
 
-var removeElement = function(nums, val) {
-  // Given an array and a number, search the array for the number and remove any instances of that number.
-  // Search 
-  // Remove 
-  // Decrement i
-  // return nums.length so the test can check to see if I did it correctly
-  for (let i = 0; i < nums.length; i++) {
-    if(nums[i] === val) {
-      nums.splice(i, 1)
+var removeDuplicates = function(nums) {
+  // Given an array in non-descending order, remove the duplicate elements in-place
+  // Iterate over the nums array
+    // If the element at index i is the same as the element at index i-1, then slice out the element at index i and decrement the value of i. 
+  // Continue iterating. Increment i. Repeat. 
+  // Returns nums/length because that's what Leetcode wants.
+  
+  for(let i = 0; i < nums.length; i++){
+    console.log('I: ', i)
+    if(nums[i]===nums[i+1]){
+      nums.splice(i+1, 1)
       i--
     }
+    // console.log('Nums: ', nums)
   }
-  console.log('Val: ', val)
-  console.log('Nums : ', nums)
+  
   return nums.length
 };
 
-// Case 1
-const nums1 = [0,1,2,2,3,0,4,2]
-const val1 = 2
-removeElement(nums1, val1)
-// Expected output = [0,1,4,0,3]
+// Runtime
+// 768 ms
 
-// Case 2
-const nums2 = [3,2,2,3]
-const val2 = 3
-removeElement(nums2, val2)
-// Expected output = [2,2]
+// // Remove Elements Problem: 
 
-// Runtime 77 ms
+// var removeElement = function(nums, val) {
+//   // Given an array and a number, search the array for the number and remove any instances of that number.
+//   // Search 
+//   // Remove 
+//   // Decrement i
+//   // return nums.length so the test can check to see if I did it correctly
+//   for (let i = 0; i < nums.length; i++) {
+//     if(nums[i] === val) {
+//       nums.splice(i, 1)
+//       i--
+//     }
+//   }
+//   // console.log('Val: ', val)
+//   // console.log('Nums : ', nums)
+//   return nums.length
+// };
+
+// // Case 1
+// const nums1 = [0,1,2,2,3,0,4,2]
+// const val1 = 2
+// removeElement(nums1, val1)
+// // Expected output = [0,1,4,0,3]
+
+// // Case 2
+// const nums2 = [3,2,2,3]
+// const val2 = 3
+// removeElement(nums2, val2)
+// // Expected output = [2,2]
+
+// // Runtime 77 ms
