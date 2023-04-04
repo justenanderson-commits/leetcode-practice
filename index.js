e3733dd8f33edf26fc6b0f4231609adc6780d9ad
Justen Anderson <justenanderson@gmail.com>
2023-04-04 10:13:18 -0600

    Add initial setup.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// Plus One
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
+// Example 3:
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+// var plusOne = function(digits) {
+    
+// };
+
+var plusOne = function(digits) {
+    
+};
+
+// var plusOne = function(digits) {
+//     return (BigInt(digits.join("")) + BigInt(1)).toString().split("");
+// };
+
+
 // Length of Last Word
 
 // Example 1:
@@ -20,8 +55,9 @@ var lengthOfLastWord = function(s) {
     // if the string length contains more than one word
     let arrayed = s.split(' ')
     let lastElement = arrayed[arrayed.length - 1]
-    console.log('Length of last Element: ', lastElement.length)
+    console.log('Length of last Element: ', lastElement)
     console.log('S: ', s)
+    return lastElement.length
 };
 
 // lengthOfLastWord('Hello World')
